6976935bb5032f51d5fbc6e6bfcbf12bb9fd9917
alisher134 <alisherrakhmanov134@gmail.com>
2024-12-01 22:38:46 +0500

    fix: edit AdminGuard

--- src/common/guards/admin.guard.ts
+++ src/common/guards/admin.guard.ts
@@ -3,7 +3,8 @@ import { Role, User } from '@prisma/client';
 
 export class AdminGuard implements CanActivate {
 	canActivate(ctx: ExecutionContext): boolean {
-		const { user } = ctx.switchToHttp().getRequest<{ user: User }>();
+		const request = ctx.switchToHttp().getRequest<{ user: User }>();
+		const user = request.user;
 
 		if (user.role !== Role.ADMIN) throw new ForbiddenException('У вас нет прав');
 
